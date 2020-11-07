8481803b4be52174c2515db80eb96f1867d41ae3
67Samuel <67ubersam@gmail.com>
2020-11-08 00:55:25 +0800

    added member_list_item.xml and circle image dependency

--- app/src/main/java/com/example/godutchleh/CreateEventDetails.java
+++ app/src/main/java/com/example/godutchleh/CreateEventDetails.java
@@ -1,10 +1,7 @@
 package com.example.godutchleh;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.view.menu.MenuBuilder;
-import androidx.appcompat.widget.Toolbar;
 
-import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -30,46 +27,13 @@ public class CreateEventDetails extends AppCompatActivity {
     Menu OverallMenu;
     Button BackArrowButton;
     TabLayout CreateEventTabs;
-    Toolbar myToolbar;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_event_details);
-        myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
-        setSupportActionBar(myToolbar);
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.action_settings:
-                // User chose the "Settings" item, show the app settings UI...
-                return true;
-
-            case R.id.my_profile:
-                // User chose the "my profile" item, go back to user profile page
-                return true;
-
-            default:
-                // If we got here, the user's action was not recognized.
-                // Invoke the superclass to handle it.
-                return super.onOptionsItemSelected(item);
-
-        }
-    }
-
-    // override method to show the icon in menu
-    @Override
-    @SuppressLint("RestrictedApi")
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu, menu);
-        if(menu instanceof MenuBuilder){
-            MenuBuilder m = (MenuBuilder) menu;
-            m.setOptionalIconsVisible(true);
-        }
-        return true;
-    }
 
 }
---
+++ app/src/main/java/com/example/godutchleh/adapters/MembersAdapter.java
@@ -0,0 +1,22 @@
+package com.example.godutchleh.adapters;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MembersAdapter {
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        //data to be shown in each view
+        private String mName;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+
+    }
+
+
+}
